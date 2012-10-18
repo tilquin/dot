1b0cd2005eee0d35c403b5934f3e4a6a92cce19c
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2012-10-18 11:21:09 -0500

    clone recursive

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,12 @@
 
 cd
 mkdir -p Library
-cd Library
-git clone git@github.com:gyllstromk/dot.git "dot"
-cd
+
+git clone git@github.com:gyllstromk/dot.git "Library/dot"
+git clone --recursive git@github.com:gyllstromk/vim.git .vim
+
 ln -s "Library/dot/etc/zshrc" .zshrc
 ln -s "Library/dot/etc/zprofile" .zprofile
 ln -s "Library/dot/etc/tmux.conf" .tmux.conf
-
-git clone git@github.com:gyllstromk/vim.git .vim
 ln -s .vim/vimrc .vimrc
 cd .vim
-git submodule init
-git submodule update
