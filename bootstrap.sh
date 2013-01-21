f42cacff8a8f621e6da70d5ce377523bb7026d78
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-01-21 09:22:00 -0600

    check for mac paths, install reattach-to-user-namespace on macs, correct linking to dot files from home

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,25 @@
 #!/bin/zsh
 
+if [[ $OSTYPE[0,6] == "darwin" ]]
+then
+	brewpath=$(which brew)
+	if [[ $brewpath == '' ]]
+	then
+		echo 'brew must be installed'
+		exit 1
+	fi
+
+	zshpath=$(which zsh)
+	if [[ $zshpath == '' ]]
+	then
+		echo 'zsh must be installed'
+		exit 1
+	fi
+
+	brew install reattach-to-user-namespace
+fi
+
+
 cd
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 rm .zshrc
@@ -9,7 +29,7 @@ mkdir -p Library
 git clone https://github.com/gyllstromk/dot.git "Library/dot"
 git clone --recursive https://github.com/gyllstromk/vim .vim
 
-for file in Library/dot/*
+for file in Library/dot/etc/*
 do
     ln -s $file .$(basename $file)
 done
