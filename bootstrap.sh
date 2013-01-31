fa848ca8d2ad6de069c601f07cd2ec6b35463171
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-01-31 08:33:15 -0600

    updated command

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,11 @@
 
 if [[ $OSTYPE[0,6] == "darwin" ]]
 then
-	command brew || { echo >&2 "brew must be installed"; exit 1; }
+	command -v brew > /dev/null 2>&1 || { echo >&2 "brew must be installed"; exit 1; }
 	brew install reattach-to-user-namespace
 fi
 
-command git || { echo >&2 "git must be installed"; exit 1; }
+command -v git > /dev/null 2>&1 || { echo >&2 "git must be installed"; exit 1; }
 
 cd
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
