2fb28adc3eab969f38da99dd5275f555756264aa
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-12-13 15:39:30 -0600

    pull git recursive

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,7 +21,7 @@ fi
 mkdir -p Library
 mkdir -p ~/.Trash
 
-git clone git@github.com:gyllstromk/dot.git "Library/dot"
+git clone --recursive git@github.com:gyllstromk/dot.git "Library/dot"
 git clone --recursive git@github.com:gyllstromk/vim.git .vim
 
 for file in Library/dot/etc/*
