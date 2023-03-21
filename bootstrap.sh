6c256593b14b83aa28ba35535584382b2d2eee03
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2023-03-20 17:35:59 -0700

    fix installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,8 @@
 if [[ $OSTYPE[0,6] == "darwin" ]]
 then
     command -v brew > /dev/null 2>&1 || { /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" }
-    brew install coreutils
-    brew install binutils
-    brew install findutils
-    brew install zsh
-    brew install ack
-    brew install vim --override-system-vi
-    brew install reattach-to-user-namespace
-    brew install clipper && brew services start clipper
-    brew install fasd
-    brew install trash
-    brew cask install iterm2
-    brew cask install flycut 
-    brew cask install flux
+    brew install coreutils binutils findutils zsh ack nvim fasd trash
+    brew install iterm2 flycut --cask
 
     defaults write -app Flycut loadOnStartup 1
     defaults write -app Flycut "ShortcutRecorder mainHotkey" '{ "keyCode" = 9 ; "modifierFlags" = 1310720; }'
@@ -24,7 +13,6 @@ fi
 command -v git > /dev/null 2>&1 || { echo >&2 "git must be installed"; exit 1; }
 
 cd
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 rm .zshrc
 
 if [[ $OSTYPE[0,5] == "linux" ]]
