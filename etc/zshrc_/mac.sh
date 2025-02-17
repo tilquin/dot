c120c7c9effa57bf23c11a796e6aba30306b2a46
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2025-02-17 09:33:50 -0800

    mac.sh

--- etc/zshrc_/mac.sh
+++ etc/zshrc_/mac.sh
@@ -1,7 +1,3 @@
-export PATH=$PATH:~/Library/Python/3.9/bin/
-
-[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
-
 # use gnu replacements for mac utils
-path=(`brew --prefix coreutils`/libexec/gnubin $path)
+path=($(brew --prefix coreutils)/libexec/gnubin $path)
 MANPATH="$(brew --prefix coreutils)/libexec/gnuman:$MANPATH"
