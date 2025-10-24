026e2ac50ce9455c586cd014987e37920069ff97
gylls <gylls@fb.com>
2025-10-24 08:25:18 -0700

    zoxide

--- etc/zshrc_/mac.sh
+++ etc/zshrc_/mac.sh
@@ -1,4 +1,4 @@
 # use gnu replacements for mac utils
 path=($(brew --prefix coreutils)/libexec/gnubin $path)
 MANPATH="$(brew --prefix coreutils)/libexec/gnuman:$MANPATH"
-[ -f /Users/gylls/homebrew/etc/profile.d/autojump.sh ] && . /Users/gylls/homebrew/etc/profile.d/autojump.sh
+eval "$(zoxide init zsh --cmd j)"
